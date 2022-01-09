669345f9f6e48c4d98cbd1598d52b0a24371c42d
Al Maz <alexmaznitskiy@gmail.com>
2022-01-09 19:15:29 +0300

    сделано 8 задание
    Финал

---
+++ DZ_8/DZ_8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace DZ_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string graph1 = "Graph1.txt";
+            string graph2 = "Graph2.txt";
+            string[] matr1;
+            string[] matr2;
+            matr1 = File.ReadAllLines(graph1);
+            matr2 = File.ReadAllLines(graph2);
+
+            bool Aftomorfizm = true;
+            for (int i = 0; i < matr1.Length; i++)
+                if (String.Compare(matr1[i], matr2[i]) != 0)
+                {
+                    Aftomorfizm = false;
+                    break;
+                }
+            if (Aftomorfizm == false)
+                Console.WriteLine("Преобразование является автоморфизмом.");
+            else 
+                Console.WriteLine("Преобразование не является автоморфизмом.");
+        }
+    }
+}
